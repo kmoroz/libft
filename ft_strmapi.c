03a99b9e37b0c1242fc531a71fbff7cf81f16802
kmoroz <axuta08@gmail.com>
2020-11-04 21:03:31 +0000

    add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strmapi.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/04 20:45:18 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/04 20:45:18 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char sub_function(unsigned int number, char c)
+{
+	return ('x');
+}
+
+int	ft_strlen(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *new_s;
+	unsigned int count;
+
+	count = 0;
+	new_s = malloc(ft_strlen(s) + 1);
+	while (s[count] != '\0')
+	{
+		new_s[count] = (*f)(count, s[count]);
+		count++;
+	}
+	if (new_s == NULL)
+		return (NULL);
+	new_s[count] = '\0';
+	return (new_s);
+}
+
+int main()
+{
+	char *result;
+	char test[] = "zhopa";
+
+	result = ft_strmapi(test, sub_function);
+}
