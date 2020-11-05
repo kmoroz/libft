702019d3f9bd3d982d6192b20a2f9915a0f31f4e
kmoroz <axuta08@gmail.com>
2020-11-05 13:33:48 +0000

    add ft_putendl_fd

---
+++ ft_putendl_fd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_putendl_fd.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/05 13:22:56 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/05 13:22:56 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	while (*s != '\0')
+	{
+		write(fd, s, 1);
+		s++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+** int main()
+** {
+** 	char why[] = "ytho";
+** 	ft_putendl_fd(why, 1);
+** }
+*/
