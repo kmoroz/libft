51bb56658a66e3e37f8aca498a457514aa8dc8cd
kmoroz <axuta08@gmail.com>
2020-11-09 11:19:43 +0000

    norm memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,10 +12,10 @@
 
 #include <stdlib.h>
 
-void *ft_memchr(const void *str, int c, size_t n)
+void	*ft_memchr(const void *str, int c, size_t n)
 {
-	size_t count;
-	char *buffer;
+	size_t	count;
+	char	*buffer;
 
 	buffer = str;
 	count = 0;
@@ -27,11 +27,12 @@ void *ft_memchr(const void *str, int c, size_t n)
 	}
 	return (NULL);
 }
-
-int main()
-{
-	char my_thoughts[] = "help";
-	char *test;
-
-	test = ft_memchr(my_thoughts, 'z', 4);
-}
+/*
+** int main()
+** {
+** 	char my_thoughts[] = "help";
+** 	char *test;
+**
+** 	test = ft_memchr(my_thoughts, 'z', 4);
+** }
+*/
