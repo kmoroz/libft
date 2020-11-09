5ae6269544cb58ff7fb8fdaa52086cb94c0123b0
kmoroz <axuta08@gmail.com>
2020-11-09 11:24:44 +0000

    norm memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_memcpy.c                                           :+:    :+:            */
+/*   ft_memcpy.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/10/31 17:00:23 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2020/10/31 17:00:23 by ksmorozo      ########   odam.nl         */
+/*   Created: 2020/11/09 11:23:09 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/09 11:23:09 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char *buffer1;
-	char *buffer2;
-	size_t count;
+	char	*buffer1;
+	char	*buffer2;
+	size_t	count;
 
 	buffer1 = src;
 	buffer2 = dest;
@@ -28,13 +28,14 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-int main()
-{
-	char source[] = "Quiz";
-	char dest[] = "Geeks";
-	char source2[] = "Quiz";
-	char dest2[] = "Geeks";
-	ft_memcpy(dest, source, 5);
-	memcpy(dest2, source2, 5);
-}
+/*
+** int main()
+** {
+** 	char source[] = "Quiz";
+** 	char dest[] = "Geeks";
+** 	char source2[] = "Quiz";
+** 	char dest2[] = "Geeks";
+** 	ft_memcpy(dest, source, 5);
+** 	memcpy(dest2, source2, 5);
+** }
+*/
