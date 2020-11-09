b0b8a4a191eb5c7847b1cafd77cafd17b66c4f3e
kmoroz <axuta08@gmail.com>
2020-11-09 11:17:11 +0000

    norm memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -12,11 +12,11 @@
 
 #include <stdlib.h>
 
-void *ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	char *buffer1;
-	char *buffer2;
-	size_t count;
+	char	*buffer1;
+	char	*buffer2;
+	size_t	count;
 
 	buffer1 = src;
 	buffer2 = dest;
@@ -34,14 +34,14 @@ void *ft_memccpy(void *dest, const void *src, int c, size_t n)
 	}
 	return (NULL);
 }
-
-int main()
-{
-	char source[] = "Quiz";
-	char dest[] = "Geeks";
-	char source2[] = "Quiz";
-	char dest2[] = "Geeks";
-	printf("%d", ft_memccpy(dest, source, 'i', 5));
-	printf("%d", memccpy(dest2, source2, 'i', 5));
-}
-
+/*
+** int main()
+** {
+** 	char source[] = "Quiz";
+** 	char dest[] = "Geeks";
+** 	char source2[] = "Quiz";
+** 	char dest2[] = "Geeks";
+** 	printf("%d", ft_memccpy(dest, source, 'i', 5));
+** 	printf("%d", memccpy(dest2, source2, 'i', 5));
+** }
+*/
