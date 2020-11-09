fc94ca7eddffe44b8596624b5ce9bbe503586142
kmoroz <axuta08@gmail.com>
2020-11-09 11:09:39 +0000

    norm isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -10,14 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isdigit(int argument)
+int	ft_isdigit(int argument)
 {
 	if (argument >= '0' && argument <= '9')
 		return (1);
 	return (0);
-}
-
-int main()
-{
-	printf("%d", ft_isdigit('5'));
 }
