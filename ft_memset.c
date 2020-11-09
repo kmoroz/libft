595b58f12f4e1cbc224c287125cd931f6935664f
kmoroz <axuta08@gmail.com>
2020-11-09 11:30:45 +0000

    norm ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_memset.c                                           :+:    :+:            */
+/*   ft_memset.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/10/28 10:48:52 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2020/10/28 10:48:52 by ksmorozo      ########   odam.nl         */
+/*   Created: 2020/11/09 11:29:07 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/09 11:29:07 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	size_t count;
-	char *buffer;
+	size_t	count;
+	char	*buffer;
 
 	count = 0;
 	buffer = str;
 	while (buffer[count] != '\0' && n != 0)
-	{	
+	{
 		buffer[count] = c;
 		count++;
 		n--;
 	}
 }
-
-int main()
-{
-	char my_thoughts[] = "help";
-	char my_thoughts2[] = "help2";
-	ft_memset(my_thoughts, '?', 6);
-	memset(my_thoughts2, '?', 6);
-}
+/*
+** int main()
+** {
+** 	char my_thoughts[] = "help";
+** 	char my_thoughts2[] = "help2";
+** 	ft_memset(my_thoughts, '?', 6);
+** 	memset(my_thoughts2, '?', 6);
+** }
+*/
