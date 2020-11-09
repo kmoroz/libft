2b9a14ac07af903ca5beaf085b82ca081d04228d
kmoroz <axuta08@gmail.com>
2020-11-09 11:40:50 +0000

    norm split

--- ft_split.c
+++ ft_split.c
@@ -1,12 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_split.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/09 11:35:51 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/09 11:35:51 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 int		get_word_count(char *str, char delimiter)
 {
-	//TODO: trim
 	int count;
 
 	count = 1;
@@ -21,8 +28,8 @@ int		get_word_count(char *str, char delimiter)
 
 char	*copy_string(char const *str, int len)
 {
-	char *new_string;
-	int count;
+	char	*new_string;
+	int		count;
 
 	count = 0;
 	new_string = malloc(len + 1);
@@ -37,41 +44,35 @@ char	*copy_string(char const *str, int len)
 
 char	**ft_split(char const *str, char dlm)
 {
-	//DECLARE VARIABLES( your double pointer to return, wordcount, current_position, count)
-	int word_count;
-	int count;
-	char **final_array;
-	char *current_position;
+	int		word_count;
+	int		count;
+	char	**final_array;
+	char	*current_position;
+	int		len;
 
-	//get word count
+	len = str - current_position;
 	word_count = get_word_count(str, dlm);
 	count = 0;
-
-	//allocate memory to your double pointer
 	final_array = malloc((word_count + 1) * sizeof(char*));
-
 	while (count < word_count)
 	{
 		current_position = str;
-
 		while (*str != dlm)
 			str++;
-		final_array[count] = copy_string(current_position, str - current_position);
+		final_array[count] = copy_string(current_position, len);
 		count++;
 		str++;
 	}
 	final_array[count] = 0;
-
 	return (final_array);
 }
-
-
-
-int main()
-{
-	char **result;
-	char str[] = "you,are,not,alone";
-	char c = ',';
-
-	result = ft_split(str, c);
-}
+/*
+** int main()
+** {
+** 	char **result;
+** 	char str[] = "you,are,not,alone";
+** 	char c = ',';
+**
+** 	result = ft_split(str, c);
+** }
+*/
