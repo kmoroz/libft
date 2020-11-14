d61b2bd45b7fbbec3f436af2428e272d12bfdd1e
kmoroz <axuta08@gmail.com>
2020-11-14 17:54:51 +0000

    edit to pass the unit test

--- ft_atoi.c
+++ ft_atoi.c
@@ -38,24 +38,34 @@ static int	is_negative(char *str)
 int			ft_atoi(const char *str)
 {
 	int count;
-	int countminus;
 	int strtonum;
 	int isnegativenum;
 
-	isnegativenum = is_negative(str);
 	count = 0;
-	countminus = 0;
 	strtonum = 0;
 	while (str[count] == ' ' || str[count] == '\n' || str[count] == '\f'
-	|| str[count] == '\r' || str[count] == '\t' || str[count] == '\v'
-	|| str[count] == '-' || str[count] == '+')
+	|| str[count] == '\r' || str[count] == '\t' || str[count] == '\v')
 		count++;
+	while (str[count] == '-' || str[count] == '+')
+		count++;
+	isnegativenum = is_negative((char *)str);
 	while (str[count] >= '0' && str[count] <= '9')
 	{
 		strtonum = 10 * strtonum + (str[count] - '0');
 		count++;
 	}
 	if (isnegativenum % 2 == 1)
-		return (strtonum * -1);
+		strtonum = strtonum * -1;
 	return (strtonum);
 }
+/*
+** int main()
+** {
+** 	char test[] = "\t\v\f\r\n \f- \f\t\n\r    06050";
+** 	char test2[] = "\t\v\f\r\n \f- \f\t\n\r    06050";
+** 	int result1;
+** 	int result2;
+** 	result1 = ft_atoi(test);
+** 	result2 = atoi(test2);
+** }
+*/
