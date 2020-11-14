f5d41f02e685a22cfd2e20f5e97f9b183a77c43f
kmoroz <axuta08@gmail.com>
2020-11-14 17:12:00 +0000

    make pass the unit test

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,40 +12,51 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+static int	is_empty(const char *s2)
+{
+	if (s2[0] == '\0')
+		return (1);
+	return (0);
+}
+
+char		*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	char	*result;
-	size_t	count_array[2];
+	size_t	count_s1;
+	size_t	count_s2;
 
-	count_array[0] = 0;
-	count_array[1] = 0;
-	while (count_array[0] < n && result == NULL)
+	count_s1 = 0;
+	count_s2 = 0;
+	result = NULL;
+	if (is_empty(s2) == 1)
+		return (s1);
+	while (count_s1 < n && result == NULL)
 	{
-		if (s1[count_array[0]] == s2[count_array[1]])
+		if (s1[count_s1] == s2[count_s2])
 		{
-			result = s1 + count_array[0];
-			while (s2[count_array[1]] != '\0' && count_array[0] < n)
+			result = (char *)s1 + count_s1;
+			while (s2[count_s2] != '\0' && count_s1 <= n)
 			{
-				if (s1[count_array[0]] != s2[count_array[1]])
+				if (s1[count_s1] != s2[count_s2])
 				{
-					count_array[1] = 0;
+					count_s2 = 0;
 					result = NULL;
 					break ;
 				}
-				count_array[0]++;
-				count_array[1]++;
+				count_s1++;
+				count_s2++;
 			}
 		}
-		count_array[0]++;
+		count_s1++;
 	}
 	return (result);
 }
 /*
 ** int main()
 ** {
-** 	char string[] = "cheesetown";
-** 	char substring[] = "town";
+** 	char string[] = "abcdef";
+** 	char substring[] = "abcdefghijklmnop";
 ** 	char *result;
-** 	result = ft_strnstr(string, substring, 11);
+** 	result = ft_strnstr(string, substring, 7);
 ** }
 */
