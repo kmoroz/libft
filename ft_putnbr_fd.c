1dde73cd32734c4c2fc55b5ecd66c6a39fce1a42
kmoroz <axuta08@gmail.com>
2020-11-16 12:36:22 +0000

    edit again

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,7 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	long number;
+
+	number = (long)n;
+	if (number < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number = number * -1;
+	}
+	if (number > 9)
+	{
+		ft_putnbr_fd(number / 10, fd);
+	}
+	ft_putchar_fd(number % 10 + '0', fd);
 }
 /*
 ** int main()
