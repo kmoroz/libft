b4eed9ff376b0a20377da781453abe50bf37cea1
kmoroz <axuta08@gmail.com>
2020-11-25 18:49:33 +0000

    initial commit

---
+++ ft_lstadd_back.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstadd_back.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/25 18:16:24 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/25 18:16:24 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last_element;
+
+	last_element = ft_lstlast(*lst);
+	if (!last_element)
+		*lst = new;
+	else
+		last_element->next = new;
+}
