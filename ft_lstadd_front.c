b4eedb608540c9eb9de9524e8a61b39510b01b43
kmoroz <axuta08@gmail.com>
2020-11-25 17:28:16 +0000

    initial commit

---
+++ ft_lstadd_front.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstadd_front.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/24 13:49:10 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/24 13:49:10 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!new || !lst)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
