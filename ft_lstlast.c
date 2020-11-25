b350735c79360f51091c230753ba8591530524b1
kmoroz <axuta08@gmail.com>
2020-11-25 18:10:00 +0000

    initial commit

---
+++ ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstlast.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/25 17:33:46 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/25 17:33:46 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list *current;
+
+	current = lst;
+	if (!current)
+		return (NULL);
+	while (current->next)
+		current = current->next;
+	return (current);
+}
