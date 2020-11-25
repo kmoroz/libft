34672bd264c265afe18e1f1bb13b9d0658635b70
kmoroz <axuta08@gmail.com>
2020-11-25 19:14:33 +0000

    initial commit

---
+++ ft_lstiter.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstiter.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/25 18:59:05 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/25 18:59:05 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list *current;
+
+	current = lst;
+	while (current)
+	{
+		(*f)(current->content);
+		current = current->next;
+	}
+}
