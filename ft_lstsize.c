eedb7b15d5e37bfffba05815dd0ee31ce5b6f776
kmoroz <axuta08@gmail.com>
2020-11-25 17:26:48 +0000

    initial commit

---
+++ ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstsize.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/25 17:10:01 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/25 17:10:01 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*current;
+
+	count = 0;
+	current = lst;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
