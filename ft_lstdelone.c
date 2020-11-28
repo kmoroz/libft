414d23e18a33c385d99209d045a9aa482f167764
kmoroz <axuta08@gmail.com>
2020-11-28 15:55:51 +0000

    initial

---
+++ ft_lstdelone.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstdelone.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/28 15:22:15 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/28 15:22:15 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	del(lst->content);
+	free(lst);
+}
