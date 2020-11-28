6cad9fefd5f61b349bcc3becc2c2db99ce9c458e
kmoroz <axuta08@gmail.com>
2020-11-28 17:25:49 +0000

    initial commit

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstclear.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/28 15:56:41 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/11/28 15:56:41 by ksmorozo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list *head;
+
+	while (*lst)
+	{
+		head = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = head;
+	}
+	*lst = NULL;
+}
