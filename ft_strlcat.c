1c291039c4a9cf689d0d044d29232da648637a0c
kmoroz <ksmorozova@hotmail.com>
2020-12-02 23:19:50 +0100

    remove main

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/01 18:39:32 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2020/11/01 18:39:32 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2020/12/02 23:19:16 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	count = 0;
 	dest_size = ft_strlen(dst);
-	src_size = ft_strlen((char *)src);
+	src_size = ft_strlen(src);
 	if (size == 0)
 		return (size + src_size);
 	while ((src[count] != '\0') && (count + dest_size < size - 1))
@@ -33,13 +33,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		return (dest_size + src_size);
 	return (size + src_size);
 }
-/*
-** int main()
-** {
-** 	char first[] = "cheese";
-** 	char second[] = "town";
-**
-** 	size_t result;
-** 	result = ft_strlcat(first, second, 10);
-** }
-*/
