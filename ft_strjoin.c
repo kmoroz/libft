635348d3d8d179121a713147c360535ad8281459
kmoroz <ksmorozova@hotmail.com>
2020-12-02 23:14:20 +0100

    remove main

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 13:44:02 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2020/11/03 13:44:02 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2020/12/02 23:10:20 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char *new_str;
 
-	new_str = (char*)malloc(ft_strlen((char*)s1) + ft_strlen((char*)s2) + 1);
+	new_str = (char*)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (new_str == NULL)
 		return (NULL);
 	ft_memcpy(new_str, s1, ft_strlen((char*)s1));
-	ft_memcpy(new_str + ft_strlen((char*)s1), s2, ft_strlen((char*)s2) + 1);
+	ft_memcpy(new_str + ft_strlen(s1), s2, ft_strlen(s2) + 1);
 	return (new_str);
 }
-/*
-** int main()
-** {
-** 	char first[] = "I hear you ";
-** 	char second[] = "call my name";
-** 	char *result;
-** 	result = ft_strjoin(first, second);
-** }
-*/
+
